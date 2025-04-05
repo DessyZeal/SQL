ff8b0c1365c7415e0d3942129356cb4860787a64
udedesire <udesire20@gmail.com>
2025-04-05 21:56:02 +0100

    new commit

--- mysql.sql
+++ mysql.sql
@@ -1,6 +1,6 @@
 -- CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, email NOT NULL);
 
--- INSERT INTO emp(empid,empname,email)
--- VALUES (5, "Fort", "test5.gmail.com");
+INSERT INTO emp(empid,empname,email)
+VALUES (5, "Fort", ".com");
 
-SELECT * FROM emp;
+-- SELECT * FROM emp;
